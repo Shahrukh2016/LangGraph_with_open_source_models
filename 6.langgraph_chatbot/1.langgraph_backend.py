03bd944644346dd30a4936acaa63b6a6037f9e95
Shahrukh Ahmad <shahrukhspn2016@gmail.com>
2025-08-25 23:43:29 +0530

    Basic Chatbot Backend commit of LangGraph

---
+++ 6.langgraph_chatbot/1.langgraph_backend.py
@@ -0,0 +1,32 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_perplexity import ChatPerplexity
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+## Defining and initiating model
+model = ChatPerplexity(model= 'sonar', api_key= os.getenv("PERPLEXITY_API_KEY"))
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+def chat_node(state: ChatState):
+    messages = state['messages']
+    response = model.invoke(messages)
+    return {"messages": [response]}
+
+# Checkpointer
+checkpointer = InMemorySaver()
+
+graph = StateGraph(ChatState)
+graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
